0dbef0ed8269407e7113699ef6efcedf1985eefc
unknown <bao.future94@gmail.com>
2021-03-24 07:54:14 +0700

    aa

--- js/main.js
+++ js/main.js
@@ -5,22 +5,26 @@ if (typeof App === "undefined") var App = {};
 $(document).ready(() => {
   // View more
   $(".viewmore").on("click", function () {
-    $("body .rever__detail--viewmore").addClass("active");
-    $("body .collapses").removeClass("hidden");
-    $("body .collapses").addClass("active");
+    $(this).parent().find('.rever__detail--viewmore').addClass('active');
+    // $("body .rever__detail--viewmore").addClass("active");
+    $(this).parent().find(".collapses").removeClass("hidden");
+    $(this).parent().find(".collapses").addClass("active");
     $(this).removeClass("active");
     $(this).addClass("hidden");
   });
   $(".collapses").on("click", function () {
-    $("body .rever__detail--viewmore").removeClass("active");
-    $("body .viewmore").removeClass("hidden");
-    $("body .viewmore").removeClass("active");
-    $("body .viewmore").addClass("active");
+    $(this).parent().find('.rever__detail--viewmore').removeClass('active');
+    // $("body .rever__detail--viewmore").removeClass("active");
+    $(this).parent().find(".viewmore").removeClass("hidden");
+    $(this).parent().find(".viewmore").removeClass("active");
+    $(this).parent().find(".viewmore").addClass("active");
     $(this).addClass("hidden");
   });
   $(".close-blue").on("click", function () {
     $("body .weonline247").addClass("close");
   });
+
+  // Sidebar right Rever 
   var s = $(".sticker");
   var pos = s.position();
   $(window).scroll(function () {
@@ -31,6 +35,7 @@ $(document).ready(() => {
       s.removeClass("stick");
     }
   });
+
   $(".cta-form.request-info").on("click", function () {
     $("body .form-247").toggleClass("active");
     $("body").append('<div class="mdialog"></div>');
@@ -40,6 +45,8 @@ $(document).ready(() => {
     $("body .form-247").removeClass("active");
     $("body .mdialog").remove();
   });
+  
+  // Carousel
   $(".carousel").owlCarousel({
     loop: true,
     margin: 10,
@@ -61,15 +68,18 @@ $(document).ready(() => {
       },
     },
   });
+
   $(".groups-select").on("click", function () {
     // $(this).find('.list').removeClass('hide');
     $(this).find(".list").show();
     $(".light-bg").show();
   });
+
   $(".light-bg").on("click", function () {
     $(".list").hide();
     $(this).hide();
   });
+
   $(".list span").on("click", function () {
     let value = $(this).text();
     let valueSelect = $(this).parent().find("~#value-select").val();
